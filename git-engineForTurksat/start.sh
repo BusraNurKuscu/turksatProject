165af526f1cbc83c872db5120ad3195044f763b1
Büşra Nur Kuşçu <191118034@samsun.edu.tr>
2020-07-17 23:18:53 +0300

    Bu repo kapatıldı

--- git-engineForTurksat/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Yolu Ayarlıyorum Büşra..."
-sleep 2
-# bu kod parçası update.sh'ın yolunu bulabilmesi için yazılmıştır.
-# path start.sh'a yani şuanki dosyaya ikinci sırada yazılıyor çünkü ilk seferde yazıldığı zaman dosya çalıştığı için bash hafızaya alırken zorlanıyor.
-sed -i "s|/c/Users/CASPER/Desktop/busra/turksat/turksatProject/|$(cd .. && pwd)/|g" update.sh
-sed -i "s|/c/Users/CASPER/Desktop/busra/turksat/turksatProject/|$(cd .. && pwd)/|g" start.sh
-bash update.sh
