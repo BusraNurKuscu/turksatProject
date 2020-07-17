d273b19fa3c7b813efd6a37a9334b979f3e13b36
Büşra Nur Kuşçu <191118034@samsun.edu.tr>
2020-07-17 21:20:23 +0300

    Yedek geri yüklendi.(17 Tem 2020 Cum 21:20:22)

--- git-engineForTurksat/git-engineForTurksat/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Yolu Ayarlıyorum Büşra..."
-sleep 2
-
-sed -i "s|/c/Users/CASPER/Desktop/busra/turksat/turksatProject/git-engineForTurksat/|$(cd .. && pwd)/|g" start.sh
-sed -i "s|/c/Users/CASPER/Desktop/busra/turksat/turksatProject/git-engineForTurksat/|$(cd .. && pwd)/|g" update.sh
-bash update.sh
